8038c4aa623d5a738e94708d4fa8919348bfacd7
Kit Rainier Oster <98862584+kitoster@users.noreply.github.com>
2023-03-31 17:24:29 -0400

    Add ufo game files

---
+++ ufo.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "ufo_functions.hpp"
+
+int main() {
+
+  greet();
+
+  std::string codeword = "motivation";
+  std::string answer = "__________";
+  int misses = 0;
+  std::vector<char> incorrect;
+  bool guess = false;
+  char letter;
+
+  
+  while (answer != codeword && misses < 8) 
+  {
+    display_misses(misses);
+    display_status(incorrect, answer);
+    std::cout << "\n\nPlease enter your guess: ";
+    std::cin >> letter;
+
+    for (int i = 0; i < codeword.length(); i++) 
+    {
+      if (letter == codeword[i]) 
+      {
+        answer[i] = letter;
+        guess = true;
+      }
+    }
+    if (guess) {
+      std::cout << "\nCorrect!\n";
+    } else 
+    {
+      std::cout << "\nIncorrect! The tractor beam pulls the person in further.\n";
+      incorrect.push_back(letter);
+      misses++;
+    }
+
+    guess = false;
+    
+  }
+
+  end_game(answer, codeword);
+
+}
