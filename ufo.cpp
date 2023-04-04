addb0aded4fd778f3484e28ebd2e5ac04b5f64d0
Kit Rainier Oster <98862584+kitoster@users.noreply.github.com>
2023-04-04 19:06:57 -0400

    Added more comments.

--- ufo.cpp
+++ ufo.cpp
@@ -1,3 +1,5 @@
+//A UFO game that operates similarly to "Hangman," but with a twist. 
+
 #include <iostream>
 #include "ufo_functions.hpp"
 
@@ -12,7 +14,7 @@ int main() {
   bool guess = false;
   char letter;
 
-  
+  //Runs input and output for each turn. 
   while (answer != codeword && misses < 8) 
   {
     display_misses(misses);
@@ -20,6 +22,7 @@ int main() {
     std::cout << "\n\nPlease enter your guess: ";
     std::cin >> letter;
 
+    //For loop in the case that the player guesses a letter correctly. Else, outputs incorrect and increases the misses counter. 
     for (int i = 0; i < codeword.length(); i++) 
     {
       if (letter == codeword[i]) 
@@ -28,7 +31,8 @@ int main() {
         guess = true;
       }
     }
-    if (guess) {
+    if (guess)
+    {
       std::cout << "\nCorrect!\n";
     } else 
     {
